0849c4d20884e10708927534f0ab239939973b84
pokapachka <pachkaosokin@yandex.ru>
2024-05-26 18:15:59 +0500

    Реализованное представление модели таймера. Осокин

---
+++ ViewModell/VMStopwatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Threading;
+using ПР41_Осокин.Modell;
+
+namespace ПР41_Осокин.ViewModell
+{
+    public class VMStopwatch : INotifyPropertyChanged
+    {
+        public Stopwatch StopWatch { get; set; }
+
+        private DispatcherTimer Timer = new DispatcherTimer()
+        {
+            Interval = new System.TimeSpan(0, 0, 1)
+        };
+
+        public VMStopwatch()
+        {
+            StopWatch = new Stopwatch() { Work = false, Time = 0 };
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            if (StopWatch.Work) StopWatch.Time++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
