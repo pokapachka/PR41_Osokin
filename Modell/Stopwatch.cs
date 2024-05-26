32681101c4fdca7b56e35537d2367e57cdec7499
pokapachka <pachkaosokin@yandex.ru>
2024-05-26 18:12:50 +0500

    Реализованная модель таймера. Осокин

---
+++ Modell/Stopwatch.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ПР41_Осокин.ViewModell;
+
+namespace ПР41_Осокин.Modell
+{
+    public class Stopwatch : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool Work;
+        private int time;
+        public int Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                OnPropertyChanged("Timer");
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public string Timer
+        {
+            get
+            {
+                float Hour = (Time / 60f / 60f);
+                float Minute = (Time / 60f) - ((int)Hour * 60f);
+                float Second = Time - (int)Hour * 60f * 60f - (int)Minute * 60f;
+                string sHour = ((int)Hour).ToString();
+                string sMinute = ((int)Minute).ToString();
+                string sSecond = ((int)Second).ToString();
+                if (Hour < 10) sHour = "0" + ((int)Hour).ToString();
+                if (Minute < 10) sMinute = "0" + ((int)Minute).ToString();
+                if (Second < 10) sSecond = "0" + ((int)Second).ToString();
+                return $"{sHour}:{sMinute}:{sSecond}";
+            }
+        }
+        private string textButton = "Начать";
+        public string TextButton
+        {
+            get { return textButton; }
+            set { textButton = value; OnPropertyChanged("TextButton"); }
+        }
+        public ObservableCollection<string> Interval { get; set; }
+        public Stopwatch() => Interval = new ObservableCollection<string>();
+        private RelayCommand startTimer;
+
+        public RelayCommand StartTimer
+        {
+            get
+            {
+                return startTimer ??
+                    (startTimer = new RelayCommand(obj =>
+                    {
+                        if (Work == false)
+                        {
+                            Interval.Clear();
+                            Time = 0;
+                            Work = true;
+                            TextButton = "Стоп";
+                        }
+                        else
+                        {
+                            Work = false;
+                            TextButton = "Начать";
+                        }
+                    }));
+            }
+        }
+        private RelayCommand intervalTimer;
+        public RelayCommand IntervalTimer
+        {
+            get
+            {
+                return intervalTimer ??
+                    (intervalTimer = new RelayCommand(obj =>
+                    {
+                        if (Work)
+                        {
+                            Interval.Insert(0, Timer);
+                        }
+                    }));
+            }
+        }
+    }
+}
